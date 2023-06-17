ae67b0c23a76466a49602bd26f8114d0321b994c
McClain Kelvin <mcclainkel@gmail.com>
2023-06-17 21:58:45 +0300

    update

--- js/intro.js
+++ js/intro.js
@@ -28,7 +28,7 @@ function loadContent(url, sectionId,contentClass) {
 
 document.querySelector('.js-load-lesson').addEventListener('click', () => {
   // loadContent('content/lessons.html', 'content','.content');
-  loadContent('content/lessons.html', 'lesson7','.lessons');
+  loadContent('content/lessons.html', 'lesson3','.lessons');
   
 })
 
