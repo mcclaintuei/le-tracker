2085d44359d766ec4edb4d507b8bdec689404492
McClain Kelvin <mcclainkel@gmail.com>
2023-08-17 00:29:33 +0300

    updated clear inicdents button

--- script.js
+++ script.js
@@ -41,9 +41,16 @@ function decrementIncident() {
 
 //Reset Hander
 document.querySelector('.js-reset').addEventListener('click', () => {
-    localStorage.removeItem('incidents')
-    totalIncidents.incidents = 0
-    renderHTML()
+    const userConfirmed = confirm('Are you sure?');
+    if (userConfirmed) {
+        console.log('Incidents Cleared');
+        localStorage.removeItem('incidents')
+        totalIncidents.incidents = 0
+        renderHTML()
+    } else {
+        console.log('Code execution cancelled.');
+    }
+   
 })
 
 
@@ -255,9 +262,9 @@ function editRecord(index) {
 
     // add the dialog box or form to the page
     document.querySelector('.records-container').appendChild(dialogBox)
-    document.querySelector('.new-count-input').addEventListener('keypress',(event)=> {
+    document.querySelector('.new-count-input').addEventListener('keypress', (event) => {
         keypressed = event.key
-        if (keypressed === 'Enter'){
+        if (keypressed === 'Enter') {
             saveNewInput();
         }
     })
@@ -329,41 +336,41 @@ let seconds = 0;
 let isRunning = false;
 
 function startTimer() {
-  clearInterval(timerInterval);
-  seconds = 0;
-  updateTimer();
-  timerInterval = setInterval(updateTimer, 1000);
-  isRunning = true;
+    clearInterval(timerInterval);
+    seconds = 0;
+    updateTimer();
+    timerInterval = setInterval(updateTimer, 1000);
+    isRunning = true;
 }
 
 function updateTimer() {
-  seconds++;
-  const hours = Math.floor(seconds / 3600);
-  const minutes = Math.floor((seconds % 3600) / 60);
-  const remainingSeconds = seconds % 60;
+    seconds++;
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
 
-  const formattedTime = `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(remainingSeconds)}`;
-  const timerElement = document.querySelector('.timer');
-  timerElement.textContent = formattedTime;
+    const formattedTime = `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(remainingSeconds)}`;
+    const timerElement = document.querySelector('.timer');
+    timerElement.textContent = formattedTime;
 
-  // Set the color based on the number of seconds
-  timerElement.style.color = getColorForSeconds(seconds);
+    // Set the color based on the number of seconds
+    timerElement.style.color = getColorForSeconds(seconds);
 }
 
 function formatTime(time) {
-  return time < 10 ? `0${time}` : time;
+    return time < 10 ? `0${time}` : time;
 }
 
 function getColorForSeconds(seconds) {
-  if (seconds < 60) {
-    return 'blue'; // Change to the desired color for seconds < 60
-  } else if (seconds < 120) {
-    return 'green'; // Change to the desired color for seconds >= 60 and < 120
-  } else if (seconds < 180) {
-    return 'orange'; // Change to the desired color for seconds >= 120 and < 180
-  } else {
-    return 'red'; // Change to the desired color for seconds >= 180
-  }
+    if (seconds < 60) {
+        return 'blue'; // Change to the desired color for seconds < 60
+    } else if (seconds < 120) {
+        return 'green'; // Change to the desired color for seconds >= 60 and < 120
+    } else if (seconds < 180) {
+        return 'orange'; // Change to the desired color for seconds >= 120 and < 180
+    } else {
+        return 'red'; // Change to the desired color for seconds >= 180
+    }
 }
 
 document.getElementById('startButton').addEventListener('click', startTimer);
